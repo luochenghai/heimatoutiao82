801062bf5d44a7b36b0d45a9deb3319c25a342be
罗成海 <1553650095@qq.com>
2019-08-31 13:47:04 +0800

    黑马头条一级导航和login页面完成

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,12 @@
     <div id="nav">
       <!-- 根组件中放入了导航和路由的承载容器 -->
       <router-link to="/">Home</router-link> |
-      <router-link to="/index"></router-link>
+      <router-link to="/login">Login</router-link>
     </div>
-    <router-view/>
+     <router-view> </router-view>
   </div>
 </template>
 
 <style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
+
 </style>
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,5 +1,28 @@
 <template>
-  <div> home </div>
+  <div class="login">
+      <!-- <div> Login </div> -->
+    <el-card class="box-card">
+        <img src="../../assets/img/logo_index.png" alt="黑马头条"/>
+        <el-form>
+            <el-form-item prop='mobile'>
+                <el-input placeholder="请输入手机号"></el-input>
+            </el-form-item>
+
+            <el-form-item prop='code'>
+                <el-input placeholder="请输入验证码" style='width:240px'></el-input>
+                <el-button style="float:right">发送验证码</el-button>
+            </el-form-item>
+
+            <el-form-item>
+                <el-checkbox :checked="true">我已经阅读并同意用户协议和隐私条款</el-checkbox>
+            </el-form-item>
+
+            <el-form-item>
+              <el-button type='primary'>登录</el-button>
+            </el-form-item>
+        </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
@@ -8,6 +31,26 @@ export default {
 }
 </script>
 
-<style>
+<style lang='less' scope>
+ .login{
+     background-image: url('../../assets/img/login_bg.jpg');
+    //占据当前可视区域全部
+    height: 100vh;
+    background-size: cover;
 
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    .box-card {
+        width:420px;
+        height: 330px;
+        img {
+            display: block;
+            width: 200px;
+            margin: 10px auto;
+        }
+    }
+
+ }
 </style>
